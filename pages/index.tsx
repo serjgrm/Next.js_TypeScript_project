30482194d47be38d8952ad99db45da80114c2954
Serhii <grammas56@gmail.com>
2023-03-18 14:06:48 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button, Htag } from '../components';
+
+export default function Home() {
+  return (
+    <div>
+      <Htag tag='h1'> Hello </Htag>
+      <Button appearance="primary">fdfd</Button>
+    </div>
+  );
+}
