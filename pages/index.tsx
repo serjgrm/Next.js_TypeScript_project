fa6b1e59602d86e650d1dc72ac8b365fa7908a22
Serhii <grammas56@gmail.com>
2023-03-20 19:04:02 +0200

    Rating tab cooperation setted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React,{useState} from "react";
 import { Button, Htag, P, Tag, Rating } from '../components';
 
 export default function Home():JSX.Element {
 
-
+const [rating, setRating] = useState<number>(4);
 
   
   return (
@@ -16,13 +16,14 @@ export default function Home():JSX.Element {
       <P size='l'>1111111</P>
       <P size="s">2222222</P>
       <P size='l'>3333333</P>
-      <Tag className='SSSSS'>sss</Tag>
-      <Tag size='s' color='ghost'>sss</Tag>
-      <Tag size='m' color='grey'>sss</Tag>
+
+      <Tag tabIndex={0} className='SSSSS'>0</Tag>
+      <Tag tabIndex={0} size='s' color='ghost'>0</Tag>
+      <Tag tabIndex={0} size='m' color='grey'>0</Tag>
       <Tag color='green'>sss</Tag>
       <Tag color='red'>sss</Tag>
       <Tag href="https://app.netlify.com/teams/serjgrm/overview?_ga=2.222979544.2145851855.1677164288-1673040163.1676992353" color='primary'>sss</Tag>
-      <Rating rating={4}></Rating>
+      <Rating rating={rating} isEditable setRating={setRating}></Rating>
 
     </div>
   );
