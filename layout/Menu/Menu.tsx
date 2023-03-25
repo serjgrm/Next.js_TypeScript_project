8b8ea454f7e9230b0c296688e6fe03903155d902
Serhii <grammas56@gmail.com>
2023-03-25 23:27:05 +0200

    Routing

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,23 +1,14 @@
 import styles from './Menu.module.css';
 import cn from 'classnames';
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { AppContext } from '../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
-import CoursesIcon from './icons/courses.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/products.svg';
-import { TopLevelCategory } from '../../interfaces/page.interface';
-import { P } from '../../components';
+
+
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { firstLevelMenu } from '@/helpers /helpers';
 
-const firstLevelMenu: FirstLevelMenuItem[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products }
-];
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
@@ -51,7 +42,7 @@ export const Menu = (): JSX.Element => {
 			</>
 		);
 	};
-
+  
 	const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
 		return (
 			<div className={styles.secondBlock}>
@@ -95,3 +86,5 @@ export const Menu = (): JSX.Element => {
 		</div>
 	);
 };
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,47 @@
-import React,{useState} from "react";
-import { Button, Htag, P, Tag, Rating } from '../components';
-import { withLayout } from "@/layout/Layout";
-import { GetStaticProps } from "next";
+import { GetStaticProps } from 'next';
+import React, { useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../components';
+import { withLayout } from '../layout/Layout';
 import axios from 'axios';
-import { MenuItem } from "@/interfaces/menu.interface";
+import { MenuItem } from '../interfaces/menu.interface';
 
+function Home({ menu }: HomeProps): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
 
-function Home({menu}:HomeProps):JSX.Element {
-
-const [rating, setRating] = useState<number>(4);
-
-  return (
-    <>
-      <Htag tag='h1'> Helloo </Htag>
-      <Button appearance='primary' arrow="right" className="dddddd" >Кнопка</Button>
-      <Button appearance="ghost" arrow="right" >Кнопка</Button>
-      <P size='l'>1111111</P>
-      <P size="s">2222222</P>
-      <P size='l'>3333333</P>
-      <Tag tabIndex={0} className='SSSSS'>0</Tag>
-      <Tag tabIndex={0} size='s' color='ghost'>0</Tag>
-      <Tag tabIndex={0} size='m' color='grey'>0</Tag>
-      <Tag color='green'>sss</Tag>
-      <Tag color='red'>sss</Tag>
-      <Tag href="https://app.netlify.com/teams/serjgrm/overview?_ga=2.222979544.2145851855.1677164288-1673040163.1676992353" color='primary'>sss</Tag>
-      <Rating rating={rating} isEditable setRating={setRating}></Rating>
-      {/* <ul>
-				{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
-			</ul> */}
-    </>
-  );
+	return (
+		<>
+			<Htag tag='h1'>Заголовок</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+			<P size='l'>Большой</P>
+			<P>Средний</P>
+			<P size='s'>Маленький</P>
+			<Tag size='s'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag color='primary'>Green</Tag>
+			<Rating rating={rating} isEditable setRating={setRating} />
+		</>
+	);
 }
-
 
 export default withLayout(Home);
 
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
 
-export async function getStaticProps() {
-  const firstCategory = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find", {
-    firstCategory
-  });
-  return {
-    props: {
-      menu,
-      firstCategory
-    }
-  };
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
 }
-
-interface HomeProps extends Record<string,unknown> {
-  menu: MenuItem[],
-  fistCategory: number; 
-}
-
-
-
-
